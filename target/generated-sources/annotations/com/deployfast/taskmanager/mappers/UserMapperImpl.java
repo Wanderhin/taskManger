404acb63bcf6b30e875cd0a0ca35e6e31cb09fda
LEUKEFACK CHRISTIAN <leukefackchristian@gmail.com>
2026-03-09 11:36:09 +0100

    apierror

---
+++ target/generated-sources/annotations/com/deployfast/taskmanager/mappers/UserMapperImpl.java
@@ -0,0 +1,31 @@
+package com.deployfast.taskmanager.mappers;
+
+import com.deployfast.taskmanager.dtos.AuthDtos;
+import com.deployfast.taskmanager.entities.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-03-09T11:07:56+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User toEntity(AuthDtos.RegisterRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.email( request.email() );
+        user.password( request.password() );
+        user.fullName( request.fullName() );
+        user.role( request.role() );
+
+        return user.build();
+    }
+}
